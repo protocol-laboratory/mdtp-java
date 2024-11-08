98ef7d9993a03652fe933c7eb6a5c618de354f58
wei <493703217@qq.com>
2024-11-08 09:45:11 +0800

    feat: add service group code
    Signed-off-by: wei <493703217@qq.com>
    Co-authored-by: ZhangJian He <shoothzj@gmail.com>

---
+++ mdtp-common/src/main/java/iot/github/protocol/mdtp/common/model/ServiceGroup.java
@@ -0,0 +1,28 @@
+package iot.github.protocol.mdtp.common.model;
+
+import lombok.Getter;
+
+@Getter
+public enum ServiceGroup {
+
+    DISCOVERY_SERVICE(1, "DISCOVERY_SERVICE"),
+
+    SECURITY_SERVICE(2, "SECURITY_SERVICE"),
+
+    CONNECTION_SERVICE(3, "CONNECTION_SERVICE"),
+
+    BILLING_SERVICE(4, "BILLING_SERVICE"),
+
+    SETTING_SERVICE(5, "SETTING_SERVICE"),
+
+    METHOD_SERVICE(6, "METHOD_SERVICE");
+
+    private final int code;
+
+    private final String description;
+
+    ServiceGroup(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
